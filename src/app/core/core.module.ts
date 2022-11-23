85f9c1e963e130bba0020b65d4a88c802a3b7f13
Tsvetan Zahariev <102287382+TsvetanZ@users.noreply.github.com>
2022-11-23 10:45:22 +0200

    create core modul and load component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GlobalLoaderComponent } from './global-loader/global-loader.component';
+
+
+
+@NgModule({
+  declarations: [
+    GlobalLoaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
